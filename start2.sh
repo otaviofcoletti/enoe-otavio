94bb1fc206fb0bf957fcaefd02311a82d3c94047
otaviofcoletti <otaviocoletti@outlook.com>
2024-08-28 20:15:33 +0000

    a

--- start2.sh
+++ start2.sh
@@ -11,7 +11,7 @@ screen -S mosquitto_session -X stuff 'mosquitto -c config-server.conf -v\n'
 # Start the third screen session with the Python subscriber in ./Server
 screen -dmS python_subscriber_session
 screen -S python_subscriber_session -X stuff "cd \"$BASE_DIR/Server\"\n"
-screen -S python_subscriber_session -X stuff 'python3 mqtt_db_control.py\n'
+screen -S python_subscriber_session -X stuff 'python3 main.py\n'
 
 # Start the fourth screen session with the Python app in ./app
 screen -dmS python_app_session
